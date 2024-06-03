aea4e44d5352e8cb0afec2f6ed4a2d6fc581e955
Norbert Jakubczak <norbert.jakubczak@sap.com>
2024-06-03 14:40:32 +0200

    Removing the #YAGNI code

--- pkg/validation/util.go
+++ pkg/validation/util.go
@@ -5,23 +5,6 @@ import (
 	"io/ioutil"
 )
 
-//func createJSON(v interface{}) ([]byte, error) {
-//	//if _, ok := v.(struct{}); !ok {
-//	//	return nil, errors.New("input is not a struct")
-//	//}
-//
-//	r := new(jsonschema.Reflector)
-//	r.ExpandedStruct = true
-//
-//	s := r.Reflect(v)
-//	jsonData, err := s.MarshalJSON()
-//	if err != nil {
-//		return nil, err
-//	}
-//
-//	return jsonData, nil
-//}
-
 func loadSchemaJSONFromFile(filePath string) ([]byte, error) {
 	schemaJSON, err := ioutil.ReadFile(filePath)
 	if err != nil {
