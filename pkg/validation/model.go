081de727a8687a2d63fe65be35d985fc60fcb386
Norbert Jakubczak <norbert.jakubczak@sap.com>
2024-06-03 14:00:16 +0200

    Shuffle the code in model.

--- pkg/validation/model.go
+++ pkg/validation/model.go
@@ -1,21 +1,43 @@
 package validation
 
-type ContentConfiguration struct {
-	Name                string                `json:"name,omitempty" yaml:"name,omitempty"`
-	LuigiConfigFragment []LuigiConfigFragment `json:"luigiConfigFragment,omitempty" yaml:"luigiConfigFragment,omitempty"`
-}
-
-type LuigiConfigFragment struct {
-	Data LuigiConfigData `json:"data,omitempty" yaml:"data,omitempty"`
-}
-
-type LuigiConfigData struct {
-	Nodes []Node `json:"nodes,omitempty" yaml:"nodes,omitempty"`
-}
-
 type Node struct {
 	EntityType  string `json:"entityType,omitempty" yaml:"entityType,omitempty"`
 	PathSegment string `json:"pathSegment,omitempty" yaml:"pathSegment,omitempty"`
 	Label       string `json:"label,omitempty" yaml:"label,omitempty"`
 	Icon        string `json:"icon,omitempty" yaml:"icon,omitempty"`
 }
+
+type LuigiConfigData struct {
+	Nodes []Node `json:"nodes,omitempty" yaml:"nodes,omitempty"`
+}
+
+type LuigiConfigFragment struct {
+	Data LuigiConfigData `json:"data,omitempty" yaml:"data,omitempty"`
+}
+
+type ContentConfiguration struct {
+	Name                string                `json:"name,omitempty" yaml:"name,omitempty"`
+	LuigiConfigFragment []LuigiConfigFragment `json:"luigiConfigFragment,omitempty" yaml:"luigiConfigFragment,omitempty"`
+}
+
+//==
+
+//type ContentConfiguration struct {
+//	Name                string                `json:"name,omitempty" yaml:"name,omitempty"`
+//	LuigiConfigFragment []LuigiConfigFragment `json:"luigiConfigFragment,omitempty" yaml:"luigiConfigFragment,omitempty"`
+//}
+//
+//type LuigiConfigFragment struct {
+//	Data LuigiConfigData `json:"data,omitempty" yaml:"data,omitempty"`
+//}
+//
+//type LuigiConfigData struct {
+//	Nodes []Node `json:"nodes,omitempty" yaml:"nodes,omitempty"`
+//}
+//
+//type Node struct {
+//	EntityType  string `json:"entityType,omitempty" yaml:"entityType,omitempty"`
+//	PathSegment string `json:"pathSegment,omitempty" yaml:"pathSegment,omitempty"`
+//	Label       string `json:"label,omitempty" yaml:"label,omitempty"`
+//	Icon        string `json:"icon,omitempty" yaml:"icon,omitempty"`
+//}
