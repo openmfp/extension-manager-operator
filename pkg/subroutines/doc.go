e1e385e74f83bcf8a8a8a8b9c8c6975955eb5bb2
Angel Kafazov <akafazov@cst-bg.net>
2025-04-11 11:48:17 +0300

    fix: update test dependencies and remove unused mockery generation file
    On-behalf-of: @SAP angel.kafazov@sap.com
    Signed-off-by: Angel Kafazov <akafazov@cst-bg.net>

--- pkg/subroutines/doc.go
+++
@@ -1,4 +0,0 @@
-package subroutines
-
-//go:generate go run -mod=mod github.com/vektra/mockery/v2@v2.53.3 --with-expecter
-//go:generate go run -mod=mod github.com/vektra/mockery/v2@v2.53.3 --srcpkg=sigs.k8s.io/controller-runtime/pkg/client --with-expecter
